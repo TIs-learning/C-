a4e5fc1871615e5717ef975080370ab4085a1f83
Louis Immanuel Arthuro <caniansreal@gmail.com>
2026-04-05 18:06:13 +0700

    Add files via upload

---
+++ assignmentcpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	// assignment
+	int a = 10;
+	
+	/*
+	Operator Assignment Penulisan singkatan 
+	var = var operator var2 
+	a -= 7;
+	a += 7;
+	a *= 6;
+	a /= 8;
+	a %= 7;
+	*/
+	return 0;
+}
