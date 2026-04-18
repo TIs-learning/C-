a61cc347f7740177e1e5d2f3d882a922d7a040b7
VarnT <caniansreal@gmail.com>
2026-04-18 20:32:13 +0700

    Add files via upload

---
+++ Printing.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // Dasar wajib
+using namespace std; // Setingan Dasar supaya gak ribet std
+
+int main() { // Pintu utama program
+	
+	// std::cout << "Halo Mode" << std::endl; ini kalo ga pake std
+	cout << "Hallo dunia" << endl;  // cout = printing
+	cout << "Kita Pergi ke bandung" << endl;
+	cin.get();
+	/*
+	endl : endline, \n. Digunakan untuk Baris baru , mengakhiri baris
+	cin: input
+	get() meminta enter
+	*/
+	return 0;
+}
